57119563e98b27a5ab681248c902177b57f6029f
Effie Mbuthi <effiembuthi@gmail.com>
2026-07-09 11:29:06 +0300

    Stop tracking .idea folder

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,45 @@
 package org.example;
 
-public class Customer {
+abstract class Customer {
+    private String customerId;
+    private String name;
+    private int age;
+    private String contact;
+    private String address;
+
+    private static int customerCounter= 0;
+
+
+    public Customer(String name, int age, String contact, String address){
+        customerCounter++;
+        this.customerId= "CUS" + String.format("%03d",customerCounter);
+        this.name=name;
+        this.age= age;
+        this.contact=contact;
+        this.address=address;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public abstract String getCustomerType();
+
+    public abstract void displayCustomerDetails();
 }
