d3efb45488d0f3d0485f79dd06292db96ccb6614
EffieMbuthi <effiembuthi@gmail.com>
2026-07-12 23:28:24 +0300

    Add TransactionManager

---
+++ bank-management-app/src/main/java/org/example/AccountManager.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class AccountManager {
+    private Account[] accounts;
+    private int accountCount;
+
+    public AccountManager() {
+        accounts = new Account[50];
+        accountCount = 0;
+    }
+
+    public void addAccount(Account account) {
+        accounts[accountCount] = account;
+        accountCount++;
+    }
+
+    public Account findAccount(String accountNumber) {
+        for (int i = 0; i < accountCount; i++) {
+            if (accounts[i].getAccountNumber().equals(accountNumber)) {
+                return accounts[i];
+            }
+        }
+        return null;
+    }
+
+    public void viewAllAccounts() {
+        for (int i = 0; i < accountCount; i++) {
+            accounts[i].displayAccountDetails();
+            System.out.println("---------------------------");
+        }
+        System.out.println("Total Accounts: " + accountCount);
+        System.out.println("Total Bank Balance: " + getTotalBalance());
+    }
+
+    public double getTotalBalance() {
+        double total = 0;
+        for (int i = 0; i < accountCount; i++) {
+            total += accounts[i].getBalance();
+        }
+        return total;
+    }
+
+    public int getAccountCount() {
+        return accountCount;
+    }
+}
